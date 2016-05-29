c0cc0dd88cf0b3740d6fadec5b41abe51e6e5ecc
hafez16 <janboofi.uoovh@gmail.com>
2016-05-29 22:53:27 +0430

    MeGa-shield v2.9

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -329,6 +329,4 @@ return {
   run = run,
   pre_process = pre_process
 }
---By @imandaneshi :)
---https://github.com/SEEDTEAM/TeleSeed/blob/test/plugins/admin.lua
----Modified by @Rondoozle for supergroups
+
