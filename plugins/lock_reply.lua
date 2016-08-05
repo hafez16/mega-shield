ef469f185f9d3a80784f0c082c55c327e0f268b5
hafez16 <janboofi.uoovh@gmail.com>
2016-08-06 02:06:35 +0430

    MeGa-shield v2.9

--- plugins/lock_reply.lua
+++ plugins/lock_reply.lua
@@ -1,37 +1,25 @@
 do
-
 local function pre_process(msg)
-    
-    --Checking mute
-    local hash = 'mate:'..msg.to.id
+    local hash = 'muter:'..msg.to.id
     if redis:get(hash) and msg.reply_id and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg) then
             delete_msg(msg.id, ok_cb, true)
             return "done"
         end
-    
         return msg
     end
-
-  
-
-
+    
 local function run(msg, matches)
     chat_id =  msg.to.id
-    
     if is_momod(msg) and matches[1] == 'lock' then
-      
-            
-                    local hash = 'mate:'..msg.to.id
+                    local hash = 'muter:'..msg.to.id
                     redis:set(hash, true)
-                    return ""
+                    return "reply has been locked"
   elseif is_momod(msg) and matches[1] == 'unlock' then
-                    local hash = 'mate:'..msg.to.id
+                    local hash = 'muter:'..msg.to.id
                     redis:del(hash)
-                    return ""
+                    return "reply has been locked"
 end
-
 end
-
 return {
     patterns = {
         '^[/!#](lock) reply$',
