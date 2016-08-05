89d73327c1a207d5ce542909fd8d5c18042d2752
hafez16 <janboofi.uoovh@gmail.com>
2016-08-06 02:10:20 +0430

    MeGa-shield v2.9

--- plugins/lock_fwd.lua
+++ plugins/lock_fwd.lua
@@ -1,37 +1,25 @@
 do
-
 local function pre_process(msg)
-    
-    --Checking mute
-    local hash = 'mate:'..msg.to.id
+    local hash = 'mutef:'..msg.to.id
     if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg)  then
             delete_msg(msg.id, ok_cb, true)
             return "done"
         end
-    
         return msg
     end
-
-  
-
-
+    
 local function run(msg, matches)
     chat_id =  msg.to.id
-    
     if is_momod(msg) and matches[1] == 'lock' then
-      
-            
-                    local hash = 'mate:'..msg.to.id
+                    local hash = 'mutef:'..msg.to.id
                     redis:set(hash, true)
-                    return ""
+                    return "forward has been locked"
   elseif is_momod(msg) and matches[1] == 'unlock' then
-                    local hash = 'mate:'..msg.to.id
+                    local hash = 'mutef:'..msg.to.id
                     redis:del(hash)
-                    return ""
+                    return "forward has been unlocked"
 end
-
 end
-
 return {
     patterns = {
         '^[/!#](lock) fwd$',
